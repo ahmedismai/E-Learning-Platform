56e74ec1e91c870aa3ce52dceacd8fb73c300e03
Ahmed Ismail <ahmedismailamer@gmail.com>
2026-05-16 02:21:45 +0300

    fix: update vite proxy target and optimize api base url for production

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://e-learning-platform-3.runasp.net/swagger/index.html",
+        target: "http://e-learning-platform-3.runasp.net/api",
         changeOrigin: true,
       },
     },
